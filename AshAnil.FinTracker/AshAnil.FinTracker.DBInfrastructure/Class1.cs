8aa8a80d391ddffa23e575d996ad0912c789c401
Peaceful Coder <sudiparora@hotmail.com>
2013-09-16 12:07:04 +0530

    Initial solution check-in

---
+++ AshAnil.FinTracker/AshAnil.FinTracker.DBInfrastructure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AshAnil.FinTracker.DBInfrastructure
+{
+    public class Class1
+    {
+    }
+}
